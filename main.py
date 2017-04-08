33c0624ea1de84601028caa42d5a1bedce7acb3a
EN10 <sfmneo@gmail.com>
2017-04-08 15:56:24 +0100

    cleanup

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
-from google.appengine.ext import ndb
-
 import webapp2
+
+class MainPage(webapp2.RequestHandler):
+  def get(self):
+    self.response.out.write("""
+    <html>
+    <body>
+      <form action="/save" method="post">
+        <div><input name="content"></div>
+        <div><input type="submit" value="Save"></div>
+      </form>
+    </body>
+    </html>""")
+
+from google.appengine.ext import ndb
 
 class IP(ndb.Model):
   ip = ndb.StringProperty()
   date = ndb.DateTimeProperty(auto_now_add=True)
-
-class MainPage(webapp2.RequestHandler):
-  def get(self):
-    self.response.out.write('<html><body>')
-    self.response.out.write("""
-      <form action="/save" method="post">
-        <div><input name="content"></div>
-        <div><input type="submit" value="Save"></div>
-    </form>
-  </body>
-  </html>""")
 
 class SubmitForm(webapp2.RequestHandler):
   def post(self):
     row = IP(ip=self.request.get('content'))
     row.put()
     self.redirect('/show')
-
 
 class DisplayIP(webapp2.RequestHandler):
   def get(self):
